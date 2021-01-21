30821369fa14f8d591262a7efaafdc768382daca
Jason Mui <Jasonmui22@gmail.com>
2021-01-21 13:51:59 -0500

    fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const {MONGODB} = require('./config')
 
 require('dotenv').config()
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({
     typeDefs,
